a71bd3642300f216cb2cf3fa25eab486abcc5706
Ryuheeeei <ryuhei.furuta@up-sider.com>
2025-09-27 11:08:26 +0900

    init: Add hocon-but-yaml-included project

---
+++ hocon-but-yaml-included/src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package io.github.ryuheeeei
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
